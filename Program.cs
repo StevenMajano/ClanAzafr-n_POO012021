1651320363d294f111e45bac84e728ffe41edb9a
StevenMajano <00064420@uca.edu.sv>
2021-03-16 19:29:24 -0600

    Changed - for a *

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace example1
+namespace Git and Github
 {
     internal class Program
     {
@@ -20,7 +20,7 @@ namespace example1
 
         public static int AddNumbers(int a, int b)
         {
-            return a - b;
+            return a * b;
         }
     }
 }
